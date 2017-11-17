2757d58488a48211a10f2e5558206da602774813
chandni chandrakant patel <ccp22@afsconnect1.njit.edu>
2017-11-17 01:27:12 -0500

    Select all records from todos done.

--- index.php
+++ index.php
@@ -9,6 +9,38 @@
 	<body>
 		<?php
 			include 'staticDatabase.php';
+			
+
+			echo '<h2>Select all Records</h2>';
+			echo '<p>Select ALL records</p>';
+			$records = todos::findAll();
+			echo "<table class=\"table table-hover\">";
+			if (count($records) > 0) {
+				echo "<thead class=\"thead-dark\">
+						<tr>
+							<th>ToDo ID</th>
+							<th>Owner Email</th>
+							<th>Owner ID</th>
+							<th>Created Date</th>
+							<th>Due Date</th>
+							<th>Message</th>
+							<th>Status</th>
+						</tr>
+					</thead>";
+				echo '<tbody>';
+				foreach ($records as $row) {
+					echo "<tr>
+							<td>".$row->id."</td>
+							<td>".$row->owneremail."</td>
+							<td>".$row->ownerid."</td>
+							<td>".$row->createddate."</td>
+							<td>".$row->duedate."</td>
+							<td>".$row->message."</td>
+							<td>".$row->isdone."</td>
+						</tr>";
+				}
+			}
+			echo "</tbody></table><hr>";
 		?>
 	</body>
 </html>
